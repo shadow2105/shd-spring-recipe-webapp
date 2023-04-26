5c02050fac4148b94dc949d912ce436108688ce5
shadow2105 <ashwinrajput66@gmail.com>
2023-04-26 10:53:16 -0400

    Initial Project Add

---
+++ src/main/java/com/example/shdspringrecipewebapp/ShdSpringRecipeWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.shdspringrecipewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShdSpringRecipeWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShdSpringRecipeWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shdspringrecipewebapp/ShdSpringRecipeWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shdspringrecipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShdSpringRecipeWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
