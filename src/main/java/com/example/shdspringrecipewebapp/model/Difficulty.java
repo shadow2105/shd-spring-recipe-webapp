4d69e51b30d5cfeeba1afd409503ae4da4b9aa53
shadow2105 <ashwinrajput66@gmail.com>
2023-05-15 17:52:15 -0400

    Add Data Model. Create JPA Entities from POJOs.

---
+++ src/main/java/com/example/shdspringrecipewebapp/model/Difficulty.java
@@ -0,0 +1,7 @@
+package com.example.shdspringrecipewebapp.model;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
---
+++ src/main/java/com/example/shdspringrecipewebapp/model/Ingredient.java
@@ -0,0 +1,62 @@
+package com.example.shdspringrecipewebapp.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private UnitOfMeasure uom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
