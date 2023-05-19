1c3d598bc2f9348b04d6e01a0656e68cfb0e102d
shadow2105 <ashwinrajput66@gmail.com>
2023-05-19 18:07:22 -0400

    Refactor Constructor to initialize Recipe

--- src/main/java/com/example/shdspringrecipewebapp/model/Ingredient.java
+++ src/main/java/com/example/shdspringrecipewebapp/model/Ingredient.java
@@ -23,10 +23,11 @@ public class Ingredient {
     public Ingredient() {
     }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
+        this.recipe = recipe;
     }
 
     public Long getId() {
